334b766464a3209a4a7b1ad25710d3119ae9ec0c
ARCassiano <anderson.cassiano@hotmail.com>
2018-08-07 20:58:14 -0300

    Incremento de comentários documentando os arquivos do projeto

--- helpers.php
+++ helpers.php
@@ -17,28 +17,39 @@
 	 */
 	function saque($placar = "0:0"){
 		
+		# Valor de retorno Default em caso de falha
 		$jogador			= "jogador a";
 		$placarVet			= explode(":", $placar);
 		$pontuacaoJogadorA	= $placarVet[0];
 		$pontuacaoJogadorB	= $placarVet[1];
 		$pontuacaoTotal		= $pontuacaoJogadorA + $pontuacaoJogadorB;	
+
+		# Varável responsável por determinar a quantidade de saques que cada jogador possui
+		# O jogo começa alternando o saque entre os jogadores a cada 5 pontos
 		$qtdSaquePorJogador	= 5;
 
+		# Quando a partida chegar em 20 a 20, o saque será alternado entre os jogadores a cada dois pontos
 		if($pontuacaoJogadorA >= 20 && $pontuacaoJogadorB >= 20)
 			$qtdSaquePorJogador	= 2;
 
-		$qtdSaque			= (int) $pontuacaoTotal / $qtdSaquePorJogador;
+		# Quantidade de vezes em que houve a troca de saque
+		$qtdSaque 	= (int) $pontuacaoTotal / $qtdSaquePorJogador;
 
-		echo "Quantidade de saques por jogador: " . $qtdSaquePorJogador . "<br>";
-		echo "Soma do placar: " . $pontuacaoTotal . "<br>";
-		echo "Quantidade de Saques utilizados: " . $qtdSaque . "<br>";
 
+		/*
+		 *	Impressão de valores para auxiliar na depuração da função
+		 *	echo "Quantidade de saques por jogador: " . $qtdSaquePorJogador . "<br>";
+		 *	echo "Soma do placar: " . $pontuacaoTotal . "<br>";
+		 *	echo "Quantidade de Saques utilizados: " . $qtdSaque . "<br>";
+		*/
+
+		# Caso a quantidade de trocas for par o saque pertencerá ao jogador a, caso contrário pertencerá ao jogador b
 		if($qtdSaque % 2 == 0)
 			$jogador 	= "jogador a";
 		else
 			$jogador 	= "jogador b";
 
-
+		# O saque da partida deve iniciar com o jogador A
 		if($placar == "0:0")
 			$jogador 	= "jogador a";
 
--- index.php
+++ index.php
@@ -17,11 +17,11 @@
 	
 	/*
 	 * Resultados esperados:
-	 *	saque("0:0"); // retorna "jogador a"
-	 *	saque("3:2"); // retorna "jogador b"
-	 *	saque("21:20"); // retorna "jogador a"
-	 *	saque("21:22"); // retorna "jogador b"
+	 *	OK - saque("0:0"); // retorna "jogador a"
+	 *	OK - saque("3:2"); // retorna "jogador b"
+	 *	OK - saque("21:20"); // retorna "jogador a"
+	 *	OK - saque("21:22"); // retorna "jogador b"
 	 */
-	echo "Parâmetro sendo passado na função saque($placar): " . $placar . "<br>";
+	#echo "Parâmetro sendo passado na função saque($placar): " . $placar . "<br>";
 	echo "O saque pertence ao " . saque($placar);
 ?>
